a9ae0eb3bc9213695fdde12a112949529dac07a4
Aleksandr22822 <alek5andr.bubnov@yandex.ru>
2024-07-07 11:56:35 +0300

    version 6.0

---
+++ SocialNetwork.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+
+using Moq;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.DAL.ENtities;
+using SocialNetwork.DAL.Repositories;
+using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.Tests
+{
+    [TestFixture]
+    public class UserRepositoryTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Mock<IUserRepository> mockUserRepository = new Mock<IUserRepository>();
+            mockUserRepository.Setups 
+           // Assert.IsFalse(mockUserRepository.F)
+           
+           // mockUserRepository.
+            
+            
+
+        }
+    }
+}
